1a1b23a285f41b31222c871dedfd6bd50872e04c
minorcell <minorcell6789@gmail.com>
2026-01-21 01:10:02 +0800

    feat: 更新猜数字游戏逻辑，添加随机数生成和胜利提示

--- 03_guess_number/guess/src/main.rs
+++ 03_guess_number/guess/src/main.rs
@@ -1,13 +1,31 @@
-use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // 标准输入输出库 // trait 接口
 
 fn main() {
     println!("猜数字！");
 
-    println!("猜测一个数字，范围是 1 到 100。");
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    loop {
+        println!("猜测一个数字，范围是 1 到 100。");
 
-    io::stdin().read_line(&mut guess).expect("读取行失败");
+        let mut guess: String = String::new();
 
-    println!("你猜测的数字是：{}", guess);
+        io::stdin().read_line(&mut guess).expect("读取行失败");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("太小了！"),
+            Ordering::Greater => println!("太大了！"),
+            Ordering::Equal => {
+                println!("你赢了！");
+                break;
+            }
+        }
+    }
 }
